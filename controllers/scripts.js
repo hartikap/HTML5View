5e918f32d00030933936db39a0dc4b354e602f10
hartikap <hartikap@gmail.com>
2015-10-27 15:08:40 +0200

    lisää tyylejä ja javascriptiä, jquery yms

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -1,1 +1,21 @@
 console.log("Here we go!");
+
+/*
+window.onload = function(event) {
+    console.log(event);
+    para1.innerHTML = "Changed from JS";
+    para1.style.backgroundColor = "red";
+}*/
+
+
+// Wait document ready event. (DOM is ready)
+$(document).ready(function(){
+    
+    console.log("jquery onload triggered");
+    $("#fuutteri").css("background-color","lightblue")
+        .css("padding","20px").css("border-radius", "8px");
+    
+    $(".about").html("<b>New text</b>");
+    $("[data-dummy]").append("Hello World!");
+    
+});
