d9f3326f209bd7b66de29ba704d5df551c26e598
hartikap <hartikap@gmail.com>
2015-10-28 11:32:05 +0200

    bootstrap mods

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -1,3 +1,4 @@
+"use strict";
 console.log("Here we go!");
 
 /*
@@ -12,10 +13,34 @@ window.onload = function(event) {
 $(document).ready(function(){
     
     console.log("jquery onload triggered");
-    $("#fuutteri").css("background-color","lightblue")
+    $("#fuutteri").css("background-color", "lightblue")
         .css("padding","20px").css("border-radius", "8px");
     
     $(".about").html("<b>New text</b>");
     $("[data-dummy]").append("Hello World!");
+    
+    var setting = {
+        
+        method:"GET",
+        url:"http://localhost:28017/oma/person/",
+        dataType:"jsonp",
+        jsonp:"jsonp"
+        
+    };
+    
+    $.ajax(setting).done(function(data){
+      
+        console.log(data);
+        console.log(data.rows[0].name);
+        for(var i=0; i<data.rows.length; i++) {
+            var html =  "<tr>"+
+                        "<td>" + data.rows[i].name + "</td>"
+                        +"<td>" + data.rows[i].address + "</td>"
+                        +"<td>" + data.rows[i].age + "</td>";
+            $(html).appendTo("tbody");
+        }
+            
+        
+    });
     
 });
